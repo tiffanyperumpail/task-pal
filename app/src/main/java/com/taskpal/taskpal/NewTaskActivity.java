31b0f6e949b87f2b130f6b8b3b9522ee584e6b62
Cameron Lopez <cslopez1@berkeley.edu>
2018-11-07 14:54:35 -0800

    even more front end

--- app/src/main/java/com/taskpal/taskpal/NewTaskActivity.java
+++ app/src/main/java/com/taskpal/taskpal/NewTaskActivity.java
@@ -2,12 +2,27 @@ package com.taskpal.taskpal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    private Button timeButton;
+    private Button difficultyButton;
+    private Button alertButton;
+    private Button dateButton;
+    private EditText titleText;
+    private EditText locationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newtask);
+        timeButton = findViewById(R.id.timeButton);
+        difficultyButton = findViewById(R.id.difficultyButton);
+        alertButton = findViewById(R.id.alertButton);
+        dateButton = findViewById(R.id.dateButton);
+        titleText = findViewById(R.id.titleText);
+        locationText = findViewById(R.id.locationText);
     }
 }
