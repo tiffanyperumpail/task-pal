f06f1fe0550d9d0894fbad6509c4c9cd43dc72ca
Tiffany Perumpail <tperumpail@berkeley.edu>
2018-12-03 15:08:23 -0800

    Add AlarmReceiver for notifications

--- app/src/main/java/com/taskpal/taskpal/AlarmReceiver.java
+++ app/src/main/java/com/taskpal/taskpal/AlarmReceiver.java
@@ -1,4 +1,20 @@
 package com.taskpal.taskpal;
 
-public class AlarmReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    String TAG = "AlarmReceiver";
+
+    public void onReceive(Context context, Intent intent) {
+        //Trigger the notification
+        NotificationScheduler.showNotification(context, NotificationActivity.class,
+                "Task Check-in", "Have you completed TASK_NAME_HERE");
+    }
+
+    public void onReceive(Intent intent) {
+
+    }
 }
